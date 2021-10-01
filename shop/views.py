70317988f972bf5d85d7d63e5c2f7b6fb7acece2
TabithaAluel <alueltabitha@gmail.com>
2021-10-01 17:41:44 +0300

    first commit

--- shop/views.py
+++ shop/views.py
@@ -13,7 +13,7 @@ def index(request):
     if item_name !='' and item_name is not None:
         product_objects=product_objects.filter(title__icontains=item_name)
 
-        #paginator
+        #paginator    
     paginator=Paginator(product_objects, 4)
     page= request.GET.get('page')
     product_objects=paginator.get_page(page)
